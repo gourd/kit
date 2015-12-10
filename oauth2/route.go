b28528d47cfd98ea66c52a34cd3c1b0f8ecae291
Koala Yeung <koalay@gmail.com>
2015-12-10 14:12:57 +0800

    [oauth2] [BREAKING] Removed RoutePat
    Will only support `Route` (router independent) in the future.

--- oauth2/route.go
+++ oauth2/route.go
@@ -1,28 +1,10 @@
 package oauth2
 
 import (
-	"github.com/gorilla/pat"
 	httpservice "github.com/gourd/kit/service/http"
 
 	"log"
 )
-
-// RoutePat adds manager's endpoint to a pat router
-// TODO: remove this function and pat dependency
-func RoutePat(rtr *pat.Router, base string, ep *Endpoints) {
-
-	// tell user where the endpoints are
-	log.Printf("OAuth2 authorize endpoint: %s/authorize", base)
-	log.Printf("OAuth2 token endpoint:     %s/token", base)
-
-	// bind handler with pat
-	rtr.Get(base+"/authorize", ep.Auth)
-	rtr.Post(base+"/authorize", ep.Auth)
-	rtr.Get(base+"/token", ep.Token)
-	rtr.Post(base+"/token", ep.Token)
-	rtr.Get(base+"/info", ep.Info)
-
-}
 
 // Route adds manager's endpoint to a router with httpservice.RouterFunc
 func Route(rfn httpservice.RouterFunc, base string, ep *Endpoints) (err error) {
