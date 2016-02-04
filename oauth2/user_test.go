af38415795b2221f128437c5d91d4df9dfc1116a
Koala Yeung <koalay@gmail.com>
2016-02-04 20:03:26 +0800

    oauth2: Test UserStore ability to store password

--- oauth2/user_test.go
+++ oauth2/user_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/gourd/kit/oauth2"
+	"github.com/gourd/kit/store"
 
 	"math/rand"
 	"testing"
@@ -75,6 +76,53 @@ func TestSetGetPassword(t *testing.T) {
 	if !u.PasswordIs(pass) {
 		t.Errorf("password is not hashed string of %#v, got %#v",
 			pass, u.Password)
+	}
+}
+
+func TestDBSavePassword(t *testing.T) {
+
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	randSeq := func(n int) string {
+		b := make([]rune, n)
+		for i := range b {
+			b[i] = letters[rand.Intn(len(letters))]
+		}
+		return string(b)
+	}
+	pass := randSeq(20)
+
+	// test store context
+	type tempKey int
+	const (
+		testDB tempKey = iota
+	)
+
+	db, err := defaultTestSrc()()
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err.Error())
+	}
+
+	us, err := oauth2.UserStoreProvider(db.Raw())
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err.Error())
+	}
+
+	u1 := &oauth2.User{Username: "TestDBSavePassword"}
+	u1.Password = u1.Hash(pass)
+
+	us.Create(nil, u1)
+
+	users := []oauth2.User{}
+	us.Search(store.NewQuery().AddCond("id", u1.ID)).All(&users)
+
+	if want, have := 1, len(users); want != have {
+		t.Errorf("expected users to have %d value, got %d instead (users=%#v)", want, have, users)
+	}
+
+	u2 := users[0]
+	if want, have := u1.Password, u2.Password; !u2.PasswordIs(pass) {
+		t.Errorf("password mismatch. expected %#v, got %#v", want, have)
 	}
 }
 
